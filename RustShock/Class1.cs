4233ce6d2df8da52afdff7208545fe2e621defc4
Lukáš Hozda <luk.hozda@gmail.com>
2017-11-26 17:04:24 +0100

    initial commit

---
+++ RustShock/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using TerrariaApi.Server;
+using TShockAPI;
+using Terraria;
+using RGiesecke.DllExport;
+
+namespace RustShock
+{
+	[ApiVersion(2,1)]
+    public class RustShock : TerrariaPlugin
+    {
+        public delegate void BasicCommand();
+        public delegate void RegisterCallback(string s, BasicCommand cb);
+
+		public RustShock(Main game) : base(game) {}
+
+		[DllImport("rust_shock.dll", CallingConvention = CallingConvention.Cdecl)]
+		public extern static void rusting();
+
+        [DllImport("rust_shock.dll", CallingConvention = CallingConvention.Cdecl)]
+        public extern static void get_cmd_register(RegisterCallback cb);
+
+        public override void Initialize() {
+            rusting();
+            get_cmd_register((s, cb) => Commands.ChatCommands.Add(new Command((_) => cb(), s)));
+        }
+        public override string Author => "magnusi";
+        public override string Name => "RustShock";
+    }
+}
